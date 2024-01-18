a72ad184fc450609ce3fddfebd7483811f2310fc
Ishmael Gyamfi <isgyamfi25@gmail.com>
2024-01-18 22:31:24 +0000

    username generate on register

--- app/Http/Requests/ProfileUpdateRequest.php
+++ app/Http/Requests/ProfileUpdateRequest.php
@@ -17,7 +17,15 @@ class ProfileUpdateRequest extends FormRequest
     {
         return [
             'name' => ['required', 'string', 'max:255'],
+            'username' => ['required', 'string', 'max:255', 'regex:/^[a-z0-9\-\.\_]+$/i'],
             'email' => ['required', 'string', 'lowercase', 'email', 'max:255', Rule::unique(User::class)->ignore($this->user()->id)],
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'regex' => 'Username can only contain alphanumeric, (.), (-) and (_)',
         ];
     }
 }
