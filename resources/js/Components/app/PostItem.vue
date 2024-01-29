23b3952e8e398f78929d07779a4690b4bf77afe2
Ishmael Gyamfi <isgyamfi25@gmail.com>
2024-01-29 16:18:03 +0000

    UI: uploading attachments

--- resources/js/Components/app/PostItem.vue
+++ resources/js/Components/app/PostItem.vue
@@ -2,10 +2,10 @@
 import { Disclosure, DisclosureButton, DisclosurePanel } from "@headlessui/vue";
 import { Menu, MenuButton, MenuItems, MenuItem } from '@headlessui/vue'
 import { PencilIcon, EllipsisVerticalIcon } from '@heroicons/vue/20/solid';
-import { TrashIcon } from "@heroicons/vue/24/solid";
+import { ChatBubbleBottomCenterTextIcon, HandThumbUpIcon, TrashIcon } from "@heroicons/vue/24/solid";
+import {isImage} from '@/helpers.js'
 
 import PostUserHeader from "@/Components/app/PostUserHeader.vue";
-import { ref } from "vue";
 import { router } from "@inertiajs/vue3";
 
 const emit = defineEmits(['editClick'])
@@ -14,10 +14,7 @@ const props = defineProps({
   post: Object,
 })
 
-const isImage = (attachment) => {
-  const mime = attachment.mime.split("/");
-  return mime[0].toLowerCase() === "image";
-};
+
 
 function openEditModal(){
     emit('editClick', props.post)
@@ -173,34 +170,13 @@ function deletePost()
       <button
         class="flex items-center justify-center flex-1 gap-1 px-4 py-2 bg-gray-100 rounded-full hover:bg-indigo-300"
       >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-          fill="currentColor"
-          class="w-6 h-6"
-        >
-          <path
-            d="M7.493 18.5c-.425 0-.82-.236-.975-.632A7.48 7.48 0 0 1 6 15.125c0-1.75.599-3.358 1.602-4.634.151-.192.373-.309.6-.397.473-.183.89-.514 1.212-.924a9.042 9.042 0 0 1 2.861-2.4c.723-.384 1.35-.956 1.653-1.715a4.498 4.498 0 0 0 .322-1.672V2.75A.75.75 0 0 1 15 2a2.25 2.25 0 0 1 2.25 2.25c0 1.152-.26 2.243-.723 3.218-.266.558.107 1.282.725 1.282h3.126c1.026 0 1.945.694 2.054 1.715.045.422.068.85.068 1.285a11.95 11.95 0 0 1-2.649 7.521c-.388.482-.987.729-1.605.729H14.23c-.483 0-.964-.078-1.423-.23l-3.114-1.04a4.501 4.501 0 0 0-1.423-.23h-.777ZM2.331 10.727a11.969 11.969 0 0 0-.831 4.398 12 12 0 0 0 .52 3.507C2.28 19.482 3.105 20 3.994 20H4.9c.445 0 .72-.498.523-.898a8.963 8.963 0 0 1-.924-3.977c0-1.708.476-3.305 1.302-4.666.245-.403-.028-.959-.5-.959H4.25c-.832 0-1.612.453-1.918 1.227Z"
-          />
-        </svg>
+      <HandThumbUpIcon class="w-6 h-6" />
         Like
       </button>
       <button
         class="flex items-center justify-center flex-1 gap-1 px-4 py-2 bg-gray-100 rounded-full hover:bg-indigo-300"
       >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-          fill="currentColor"
-          class="w-6 h-6"
-        >
-          <path
-            d="M4.913 2.658c2.075-.27 4.19-.408 6.337-.408 2.147 0 4.262.139 6.337.408 1.922.25 3.291 1.861 3.405 3.727a4.403 4.403 0 0 0-1.032-.211 50.89 50.89 0 0 0-8.42 0c-2.358.196-4.04 2.19-4.04 4.434v4.286a4.47 4.47 0 0 0 2.433 3.984L7.28 21.53A.75.75 0 0 1 6 21v-4.03a48.527 48.527 0 0 1-1.087-.128C2.905 16.58 1.5 14.833 1.5 12.862V6.638c0-1.97 1.405-3.718 3.413-3.979Z"
-          />
-          <path
-            d="M15.75 7.5c-1.376 0-2.739.057-4.086.169C10.124 7.797 9 9.103 9 10.609v4.285c0 1.507 1.128 2.814 2.67 2.94 1.243.102 2.5.157 3.768.165l2.782 2.781a.75.75 0 0 0 1.28-.53v-2.39l.33-.026c1.542-.125 2.67-1.433 2.67-2.94v-4.286c0-1.505-1.125-2.811-2.664-2.94A49.392 49.392 0 0 0 15.75 7.5Z"
-          />
-        </svg>
+        <ChatBubbleBottomCenterTextIcon class="w-6 h-6"/>
         Comment
       </button>
     </div>
--- resources/js/Components/app/PostModal.vue
+++ resources/js/Components/app/PostModal.vue
@@ -1,71 +1,77 @@
 <script setup>
 import { computed, onMounted, ref, watch, onUpdated, reactive } from 'vue'
 import {
-  TransitionRoot,
-  TransitionChild,
-  Dialog,
-  DialogPanel,
-  DialogTitle,
+    TransitionRoot,
+    TransitionChild,
+    Dialog,
+    DialogPanel,
+    DialogTitle,
 } from '@headlessui/vue'
 import TextAreaInput from '@/Components/TextAreaInput.vue';
 import PostUserHeader from '@/Components/app/PostUserHeader.vue';
-import { XMarkIcon } from '@heroicons/vue/24/solid';
+import { DocumentIcon, XMarkIcon } from '@heroicons/vue/24/solid';
 import { useForm } from '@inertiajs/vue3';
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import { PaperClipIcon } from '@heroicons/vue/24/solid';
+import { isImage } from '@/helpers';
 // import BalloonEditor from '@ckeditor/ckeditor5-build-balloon';
 
 
 const editor = ClassicEditor
 const editorConfig = {
-    toolbar: ['heading', 'bold', 'italic', '|', 'undo', '|',  'link', 'numberedList', 'bulletedList', 'blockQuote', '|', 'outdent', 'indent' ],
-    balloonToolbar: ['heading', 'bold', 'italic', '|', 'undo', '|',  'link', 'numberedList', 'bulletedList', 'blockQuote', '|', 'outdent', 'indent' ]
+    toolbar: ['heading', 'bold', 'italic', '|', 'undo', '|', 'link', 'numberedList', 'bulletedList', 'blockQuote', '|', 'outdent', 'indent'],
+    balloonToolbar: ['heading', 'bold', 'italic', '|', 'undo', '|', 'link', 'numberedList', 'bulletedList', 'blockQuote', '|', 'outdent', 'indent']
 }
 
 const props = defineProps({
-  post: {
-    type: Object,
-    required: true
-  },
-  modelValue: Boolean
+    post: {
+        type: Object,
+        required: true
+    },
+    modelValue: Boolean
 })
 
+/**
+ *
+ * {
+ *  file: File,
+ *  url: '',
+ * }
+ * */
+const attachmentFiles = ref([])
+
 const form = useForm({
-  id: null,
-  body: ''
+    id: null,
+    body: ''
 })
 
 
 const show = computed({
-  get: () => props.modelValue,
-  set: (value) => emit('update:modelValue', value)
+    get: () => props.modelValue,
+    set: (value) => emit('update:modelValue', value)
 })
 
 const emit = defineEmits(['update:modelValue'])
 
 watch(() => props.post, () => {
-  form.id = props.post.id
-  form.body = props.post.body
+    form.id = props.post.id
+    form.body = props.post.body
 })
 
-// onUpdated(() => {
-//   console.log('***************************************************');
-//   console.log("Updated");
-//   console.log('***************************************************');
-// }),
+const closeModal = () => {
+    show.value = false
+    form.reset()
+    attachmentFiles.value = []
+}
 
 function update() {
-//   const form = useForm({
-//     id: props.post.id,
-//     body: props.post.body,
-//   })
 
-    if(form.id)
-    {
+    if (form.id) {
         form.put(route('post.update', props.post.id), {
             preserveScroll: true,
             onSuccess: () => {
-            show.value = false
-            form.reset()
+                show.value = false
+                form.reset()
             }
         })
     } else {
@@ -77,60 +83,113 @@ function update() {
             }
         })
     }
-
-
 }
 
-// onMounted(() => {
-//   console.log('***************************************************');
-//   console.log(props.post);
-//   console.log('***************************************************');
-// })
+async function onFileAttached($event) {
+    for (const file of $event.target.files) {
+        const atFile = {
+            file,
+            url: await readFile(file)
+        }
+        attachmentFiles.value.push(atFile)
+    }
+
+    $event.target.value = null
+    console.log(attachmentFiles.value);
+}
+
+async function readFile(file) {
+    return new Promise((res, rej) => {
+        if (isImage(file)) {
+            const reader = new FileReader()
+
+            reader.onload = () => {
+                res(reader.result)
+            }
+
+            reader.onerror = rej
+
+            reader.readAsDataURL(file)
+        } else {
+            res(null)
+        }
+    })
+}
+
+function removeFile(atFile) {
+  attachmentFiles.value = attachmentFiles.value.filter(f => f !== atFile)
+}
 
 </script>
 
 <template>
-  <teleport to='body'>
-    <TransitionRoot appear :show="show" as="template">
-      <Dialog as="div" class="relative z-10">
-        <TransitionChild as="template" enter="duration-300 ease-out" enter-from="opacity-0" enter-to="opacity-100"
-          leave="duration-200 ease-in" leave-from="opacity-100" leave-to="opacity-0">
-          <div class="fixed inset-0 bg-black/25" />
-        </TransitionChild>
+    <teleport to='body'>
+        <TransitionRoot appear :show="show" as="template">
+            <Dialog as="div" class="relative z-10">
+                <TransitionChild as="template" enter="duration-300 ease-out" enter-from="opacity-0" enter-to="opacity-100"
+                    leave="duration-200 ease-in" leave-from="opacity-100" leave-to="opacity-0">
+                    <div class="fixed inset-0 bg-black/25" />
+                </TransitionChild>
 
-        <div class="fixed inset-0 overflow-y-auto">
-          <div class="flex items-center justify-center min-h-full p-4 text-center">
-            <TransitionChild as="template" enter="duration-300 ease-out" enter-from="opacity-0 scale-95"
-              enter-to="opacity-100 scale-100" leave="duration-200 ease-in" leave-from="opacity-100 scale-100"
-              leave-to="opacity-0 scale-95">
-              <DialogPanel
-                class="w-full max-w-md p-6 overflow-hidden text-left align-middle transition-all transform bg-white shadow-xl rounded-2xl">
-                <DialogTitle as="h3"
-                  class="flex items-center justify-between px-4 py-3 text-lg font-medium leading-6 text-gray-900 bg-gray-200 rounded-lg">
-                  {{ form.id ? 'EDIT POST' : 'CREATE NEW POST' }}
-                  <button @click="show = false"
-                    class="flex items-center justify-center px-2 py-2 text-sm transition rounded-full hover:bg-black/10">
-                    <XMarkIcon class="w-4 h-4 " />
-                  </button>
-                </DialogTitle>
-                <div class="px-4 py-3 mt-2">
-                  <PostUserHeader :post="post" :show-time="false" class="mb-3" />
-                  <ckeditor :editor="editor" v-model="form.body" :config="editorConfig"></ckeditor>
-                  <!-- <TextAreaInput v-model="form.body" row="5" class="w-full mb-3" /> -->
-                </div>
+                <div class="fixed inset-0 overflow-y-auto">
+                    <div class="flex items-center justify-center min-h-full p-4 text-center">
+                        <TransitionChild as="template" enter="duration-300 ease-out" enter-from="opacity-0 scale-95"
+                            enter-to="opacity-100 scale-100" leave="duration-200 ease-in" leave-from="opacity-100 scale-100"
+                            leave-to="opacity-0 scale-95">
+                            <DialogPanel
+                                class="w-full max-w-md p-6 overflow-hidden text-left align-middle transition-all transform bg-white shadow-xl rounded-2xl">
+                                <DialogTitle as="h3"
+                                    class="flex items-center justify-between px-4 py-3 text-lg font-medium leading-6 text-gray-900 bg-gray-200 rounded-lg">
+                                    {{ form.id ? 'EDIT POST' : 'CREATE NEW POST' }}
+                                    <button @click="closeModal"
+                                        class="flex items-center justify-center px-2 py-2 text-sm transition rounded-full hover:bg-black/10">
+                                        <XMarkIcon class="w-4 h-4 " />
+                                    </button>
+                                </DialogTitle>
+                                <div class="px-4 py-3 mt-2">
+                                    <PostUserHeader :post="post" :show-time="false" class="mb-3" />
+                                    <ckeditor :editor="editor" v-model="form.body" :config="editorConfig"></ckeditor>
+                                    <div class="grid grid-cols-2 gap-3 my-3 lg:grid-cols-3">
+                                        <template v-for="atFile of attachmentFiles" :key="atFile.id">
+                                            <div
+                                                class="relative flex flex-col items-center justify-center text-gray-500 bg-blue-200 rounded group aspect-square">
+                                                <button @click="removeFile(atFile)"
+                                                    class="absolute flex items-center p-2 text-xs text-white rounded-full opacity-0 right-1 top-1 bg-red-700/40 group-hover:opacity-100 hover:bg-red-900/80">
+                                                    <XMarkIcon class="w-5 h-5" />
+                                                    <!-- Cancel Avatar -->
+                                                </button>
+                                                <img v-if="isImage(atFile.file)" :src="atFile.url"
+                                                    class="object-cover rounded aspect-square" />
 
-                <div class="px-4 py-3 mt-4">
-                  <button type="button"
-                    class="w-full px-3 py-2 text-sm font-semibold text-gray-700 bg-indigo-200 rounded-full shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                    @click="update">
-                    Save
-                  </button>
+                                                <template v-else>
+                                                    <!-- file -->
+                                                    <DocumentIcon class="w-12 h-12 mb-3" />
+                                                    <small class="text-center">{{ atFile.file.name }}</small>
+                                                </template>
+                                            </div>
+                                        </template>
+                                    </div>
+                                </div>
+
+                                <div class="flex gap-2 px-4 py-3 mt-4">
+                                    <button type="button"
+                                        class="relative w-20 justify-center items-center flex rounded-full border-none px-2.5 py-1.5 text-sm font-semibold text-gray-900 hover:bg-gray-50">
+                                        <PaperClipIcon class="w-6 h-6 mr-2" />
+
+                                        <input @click.stop @change="onFileAttached" type="file" multiple
+                                            class="absolute top-0 bottom-0 left-0 right-0 opacity-0" />
+                                    </button>
+                                    <button type="button"
+                                        class="w-20 px-3 py-2 text-sm font-semibold text-gray-700 bg-indigo-200 rounded-full shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                                        @click="update">
+                                        Save
+                                    </button>
+                                </div>
+                            </DialogPanel>
+                        </TransitionChild>
+                    </div>
                 </div>
-              </DialogPanel>
-            </TransitionChild>
-          </div>
-        </div>
-      </Dialog>
-    </TransitionRoot>
-  </teleport>
+            </Dialog>
+        </TransitionRoot>
+    </teleport>
 </template>
